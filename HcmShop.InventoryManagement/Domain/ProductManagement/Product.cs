f883fdfb5d630bad4c178d20e5473edc9eff94f1
HACHEM WAFAA <wafaahachem1431@gmail.com>
2024-11-01 23:33:30 +0100

    Add project files.

---
+++ HcmShop.InventoryManagement/Domain/ProductManagement/Product.cs
@@ -0,0 +1,202 @@
+﻿using HcmShop.InventoryManagement.Domain.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HcmShop.InventoryManagement.Domain.ProductManagement
+{
+    public class Product
+    {
+
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        private int maxItemsInStock = 0;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value.Length > 50 ? value[..50] : value;
+            }
+        }
+
+        public string? Description
+        {
+            get { return description; }
+            set
+            {
+                if (value == null)
+                {
+                    description = string.Empty;
+                }
+                else
+                {
+                    description = value.Length > 250 ? value[..250] : value;
+
+                }
+            }
+        }
+
+        public UnitType UnitType { get; set; }
+        public int AmountInStock { get; private set; }
+        public bool IsBelowStockThreshold { get; private set; }
+        public Price Price {  get; set; }
+
+        public Product(int id) : this(id, string.Empty)
+        {
+
+        }
+
+        public Product(int Id, string name)
+        {
+            this.Id = Id;
+            Name = name;
+        }
+
+        public Product(int id, string name, string? description, Price price, UnitType unitType, int maxAmountInStock)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            UnitType = unitType;
+
+            maxItemsInStock = maxAmountInStock;
+
+            UpdateLowStock();
+        }
+
+
+        public void UseProduct(int items)
+        {
+            if (items <= AmountInStock)
+            {
+                //use the items
+                AmountInStock -= items;
+
+                UpdateLowStock();
+
+                Log($"Amount in stock updated. Now {AmountInStock} items in stock.");
+            }
+            else
+            {
+                Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}. {AmountInStock} available but {items} requested.");
+            }
+        }
+
+        public void IncreaseStock()
+        {
+            AmountInStock++;
+        }
+
+        public void IncreaseStock(int amount)
+        {
+            int newStock = AmountInStock + amount;
+
+            if (newStock <= maxItemsInStock)
+            {
+                AmountInStock += amount;
+            }
+            else
+            {
+                AmountInStock = maxItemsInStock;//we only store the possible items, overstock isn't stored
+                Log($"{CreateSimpleProductRepresentation} stock overflow. {newStock - AmountInStock} item(s) ordere that couldn't be stored.");
+            }
+
+            if (AmountInStock > 10)
+            {
+                IsBelowStockThreshold = false;
+            }
+        }
+
+        private void DecreaseStock(int items, string reason)
+        {
+            if (items <= AmountInStock)
+            {
+                //decrease the stock with the specified number items
+                AmountInStock -= items;
+            }
+            else
+            {
+                AmountInStock = 0;
+            }
+
+            UpdateLowStock();
+
+            Log(reason);
+        }
+
+        public string DisplayDetailsShort()
+        {
+            return $"{Id}. {Name} \n{AmountInStock} items in stock";
+        }
+
+        public string DisplayDetailsFull()
+        {
+            StringBuilder sb = new();
+
+            sb.Append($"{Id} {Name} \n{Description}\n{Price}\n{AmountInStock} item(s) in stock");
+
+            if (IsBelowStockThreshold)
+            {
+                sb.Append("\n!!STOCK LOW!!");
+            }
+
+            return sb.ToString();
+
+        }
+
+        public string DisplayDetailsFull(string extraDetails)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{Id} {Name} \n{Description}\n{Price}\n{AmountInStock} item(s) in stock");
+
+            sb.Append(extraDetails);
+
+            if (IsBelowStockThreshold)
+            {
+                sb.Append("\n!!STOCK LOW!!");
+            }
+
+            return sb.ToString();
+        }
+
+        private void UpdateLowStock()
+        {
+            if (AmountInStock < 10)
+            {
+                IsBelowStockThreshold = true;
+            }
+        }
+
+        private void Log(string message)
+        {
+
+            Console.WriteLine(message);
+        }
+
+        private string CreateSimpleProductRepresentation()
+        {
+            return $"Product {Id} ({Name})";
+        }
+
+
+    }
+}
+
+
---
+++ HcmShop.InventoryManagement/Domain/ProductManagement/UnitType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HcmShop.InventoryManagement.Domain.ProductManagement
+{
+    public enum UnitType           //nous créons un type d'énumération, qui est une liste de valeurs nommées.
+    {
+        perItem,
+        perBox,
+        perKg
+    }
+}
