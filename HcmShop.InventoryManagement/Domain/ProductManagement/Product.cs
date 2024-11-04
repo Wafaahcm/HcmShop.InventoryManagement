5590f9226177c79e7823387a8e4c53208669d625
HACHEM WAFAA <wafaahachem1431@gmail.com>
2024-11-04 10:46:52 +0100

    Add Partial class

--- HcmShop.InventoryManagement/Domain/ProductManagement/Product.cs
+++ HcmShop.InventoryManagement/Domain/ProductManagement/Product.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HcmShop.InventoryManagement.Domain.ProductManagement
 {
-    public class Product
+    public partial class Product
     {
 
         private int id;
@@ -176,24 +176,7 @@ namespace HcmShop.InventoryManagement.Domain.ProductManagement
             return sb.ToString();
         }
 
-        private void UpdateLowStock()
-        {
-            if (AmountInStock < 10)
-            {
-                IsBelowStockThreshold = true;
-            }
-        }
-
-        private void Log(string message)
-        {
-
-            Console.WriteLine(message);
-        }
-
-        private string CreateSimpleProductRepresentation()
-        {
-            return $"Product {Id} ({Name})";
-        }
+        
 
 
     }
---
+++ HcmShop.InventoryManagement/Domain/ProductManagement/ProductPart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HcmShop.InventoryManagement.Domain.ProductManagement
+{
+    public partial class Product
+    {
+        private void UpdateLowStock()
+        {
+            if (AmountInStock < 10)
+            {
+                IsBelowStockThreshold = true;
+            }
+        }
+
+        private void Log(string message)
+        {
+
+            Console.WriteLine(message);
+        }
+
+        private string CreateSimpleProductRepresentation()
+        {
+            return $"Product {Id} ({Name})";
+        }
+    }
+}
